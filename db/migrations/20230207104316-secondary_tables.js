48eeeb1f70251d3e9eacb93851f9df2bfe7e9964
nicolalew <nicola.lew@gmail.com>
2023-02-07 20:27:13 +0800

    users and user_addresses

---
+++ db/migrations/20230207104316-secondary_tables.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user_addresses", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      type: {
+        type: Sequelize.ENUM("mailing", "billing"),
+        defaultValue: "mailing",
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("user_addresses");
+  },
+};
