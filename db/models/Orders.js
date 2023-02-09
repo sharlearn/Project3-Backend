837c631303044741d030483bb229a550e420c8f6
sharlearn <liewsharlen@gmail.com>
2023-02-10 00:34:59 +0800

    Fixed typos in models.

--- db/models/Orders.js
+++ db/models/Orders.js
@@ -4,14 +4,14 @@ module.exports = (sequelize, DataTypes) => {
   class Orders extends Model {
     static associate(models) {
       this.belongsTo(models.user, { as: "buyer" });
-      this.belongsTo(models.user_addresses, { as: "delivery_address" });
+      // this.belongsTo(models.user_addresses, { as: "delivery_address" });
       this.belongsToMany(models.design, { through: "ordered_designs" });
     }
   }
   Orders.init(
     {
       total_price: DataTypes.INTEGER,
-      delivery_address: DataTypes.INT,
+      delivery_address: DataTypes.INTEGER,
     },
     {
       sequelize,
